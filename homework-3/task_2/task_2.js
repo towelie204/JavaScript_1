9799a5fa860fe4732f81965309e3c7324a0e889c
towelie <towelie004@gmail.com>
2020-06-16 04:08:58 +0300

    homework 3

---
+++ homework-3/task_2/task_2.js
@@ -0,0 +1,31 @@
+'use strict';
+
+const post = {
+    author: 'John',
+    postId: 23,
+    comments: [
+        {
+            userId: 10,
+            userName: 'Alex',
+            text: 'lorem ipsum',
+            rating: {
+                likes: 10,
+                dislikes: 2
+            }
+        },
+        {
+            userId: 5,
+            userName: 'Jane',
+            text: 'lorem ipsum 2',
+            rating: {
+                likes: 3,
+                dislikes: 1
+            }
+        },
+    ]
+}
+
+console.log(post.author);
+console.log(post.comments[0].rating.dislikes);
+console.log(post.comments[1].userId);
+console.log(post.comments[1].text);
